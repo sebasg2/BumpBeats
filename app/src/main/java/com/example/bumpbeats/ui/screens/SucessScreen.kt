7867181cc5560a53932a5eba0562d3b926f8e8e9
sebasg2 <149262899+sebasg2@users.noreply.github.com>
2024-11-30 17:03:00 +0100

    changed the heartrate screen to work with the bpms, not fake data

--- app/src/main/java/com/example/bumpbeats/ui/screens/SucessScreen.kt
+++ app/src/main/java/com/example/bumpbeats/ui/screens/SucessScreen.kt
@@ -2,29 +2,20 @@ package com.example.bumpbeats.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.bumpbeats.ui.theme.BumpBeatsTheme
 
 @Composable
-fun SuccessScreen() {
+fun SuccessScreen(onNavigateToHeartRate: () -> Unit) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Sign-up Successful! Welcome to BumpBeats!")
+        Button(onClick = onNavigateToHeartRate) {
+            Text(text = "Go to Heart Rate Monitor")
+        }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun SuccessScreenPreview() {
-    BumpBeatsTheme {
-        SuccessScreen()
-    }
-}
-
-
